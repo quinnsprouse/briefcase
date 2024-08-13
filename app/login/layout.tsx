a35dc214b6c98cbc2d2ab221affbd88c39bcfdee
quinnsprouse <qsprouse@redi.health>
2024-08-13 12:25:03 -0400

    project init

---
+++ app/login/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Login to Briefcase",
+  description: "create a breifcase account",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="container mx-auto py-20">{children}</div>
+      </body>
+    </html>
+  );
+}
